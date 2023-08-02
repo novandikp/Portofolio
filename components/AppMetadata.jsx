311395a530a6169bbf2f2b4c661b3d6ddc87529d
Novandi Kevin Pratama <kevinpret27@gmail.com>
2023-08-02 07:16:01 +0700

    Update metadata

--- components/AppMetadata.jsx
+++ components/AppMetadata.jsx
@@ -1,9 +1,9 @@
 const author = "Novandi Kevin Pratama";
 const description =
 	"Software developer from Moldova, Republic of, who loves to develop beautiful websites, single page applications, customer relationship management or code from scratch using React and Next.js";
-const url = "https://vasile-novatchii.netlify.app";
+const url = "https://https://novandikp.vercel.app";
 export const AppMetadata = {
-	metadataBase: new URL("https://vasile-novatchii.netlify.app/"),
+	metadataBase: new URL("https://https://novandikp.vercel.app/"),
 	title: {
 		default: `Portfolio | ${author}`,
 		template: `%s | ${author}`
@@ -30,13 +30,13 @@ export const AppMetadata = {
 		siteName: `${author} | Portfolio`,
 		images: [
 			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
+				url: "https://https://novandikp.vercel.app/screenshot.webp",
 				width: 800,
 				height: 600,
 				alt: "My personal portfolio website"
 			},
 			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
+				url: "https://https://novandikp.vercel.app/screenshot.webp",
 				width: 1800,
 				height: 1600,
 				alt: "My personal portfolio website"
